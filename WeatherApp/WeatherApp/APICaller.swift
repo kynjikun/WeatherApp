3d4c7d7b9568394a1b5f3b7bb4fc9281c56276ca
Kynji Pepito <kynjikun@gmail.com>
2023-10-14 15:41:47 +0800

    initial commit
    - with wip APICaller

---
+++ WeatherApp/WeatherApp/APICaller.swift
@@ -0,0 +1,49 @@
+//
+//  APICaller.swift
+//  WeatherApp
+//
+//  Created by Kynji Kyle Niño A. Pepito on 10/14/23.
+//
+
+import Foundation
+
+enum ExcludeOpts {
+    case Current
+    case Minutely
+    case Hourly
+    case Daily
+    case Alert
+    
+    var toString: String {
+        switch self {
+            case .Current: "current"
+            case .Minutely: "minute"
+            case .Hourly: "hourly"
+            case .Daily: "daily"
+            case .Alert: "alert"
+        }
+    }
+}
+
+class APICaller {
+    static let key = "55edb762352afab7c34d25e00927bdd4" // temp
+    static let shared = APICaller()
+    
+    func fetchCurrentWeather(lat: Double, lon: Double, excludes: [ExcludeOpts]) async throws -> String { // temp return needs ot convert JSON to current weather model
+        let url = URL(string: "https://api.openweathermap.org/data/3.0/onecall?lat=\(lat)&lon=\(lon).04&exclude=\(String(describing: excludes.optsToString))&appid=\(APICaller.key)")
+        
+        let request = URLRequest(url: url!)
+        let (data, _) = try await URLSession.shared.data(for: request)
+//        let fetchedData = try JSONDecoder().decode(<Model>, from: try mapResponse(response: (data,response)))
+        
+        let resp = String(data: data, encoding: .utf8) ?? ""
+
+        return resp
+    }
+}
+
+extension Array where Element == ExcludeOpts {
+    func optsToString() -> String {
+        self.reduce("") { result, current in result + "\(result.isEmpty ? current.toString : "," + current.toString)" }
+    }
+}
---
+++ WeatherApp/WeatherApp/WeatherModel.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherModel.swift
+//  WeatherApp
+//
+//  Created by Kynji Kyle Niño A. Pepito on 10/14/23.
+//
+
+import Foundation
+
+enum TimesOfDay {
+    case dawn
+    case sunrise
+    case midday
+    case sunset
+    case dusk
+    case midnight
+}
+
+struct Weather {
+    
+    var date: NSDate
+}
