c3d25020a3f183a41e9ff0dafa3e7524172a19a7
Kynji Pepito <kynjikun@gmail.com>
2023-10-15 11:30:13 +0800

    Edit WeatherModel
    - updated Weather properties
    - added new class JsonFormatter for parsing success returns to Weather model format value

--- WeatherApp/WeatherApp/WeatherModel.swift
+++ WeatherApp/WeatherApp/WeatherModel.swift
@@ -7,16 +7,23 @@
 
 import Foundation
 
-enum TimesOfDay {
-    case dawn
-    case sunrise
-    case midday
-    case sunset
-    case dusk
-    case midnight
+struct Weather: Decodable {
+
+    let status: String
+    let description: String
+    let icon: String
 }
 
-struct Weather {
-    
-    var date: NSDate
+class JsonFormatter {
+    func map(_ keyVal: [String: Any]) -> Weather? {
+        guard let current = keyVal["current"] as? [String: Any] else { return nil }
+        guard let weather = current["weather"] as? [[String: AnyObject]] else { return nil }
+
+        guard let status = weather[0]["main"] as? String else { return nil }
+        guard let description = weather[0]["description"] as? String else { return nil }
+        guard let icon = weather[0]["icon"] as? String else { return nil }
+        
+        return Weather(status: status, description: description, icon: icon)
+    }
 }
+
